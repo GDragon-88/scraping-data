295b5f368766f2fb9a26d8fb357344099eb357e3
keshi <manhchienltpt@gmail.com>
2023-05-15 22:29:10 +0700

    add

--- main.js
+++ main.js
@@ -1,33 +1,62 @@
 const express = require("express");
 const app = express()
-const PORT = 3000
-app.listen(PORT,()=>{
-    console.log(PORT);
+const PORT = 3002
+
+
+
+
+const puppeteer = require('puppeteer-extra')
+const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha')
+puppeteer.use(
+    RecaptchaPlugin({
+        provider: {
+            id: '2captcha',
+            token: "3915eebb0fc0ed86495a9f32fa90ea4e",
+
+        },
+        visualFeedback: true // colorize reCAPTCHAs (violet = detected, green = solved)
+    })
+)
+puppeteer.launch({ headless: true }).then(async browser => {
+    try {
+        const page = await browser.newPage()
+        await page.goto('https://portal.threetrader.com/login')
+
+
+        // const setInputValue = ()=>{
+        //  document.querySelector("input").value ="broker@p2t.sg"
+        //  document.querySelector("input")[1].value ="D6i7vcZG"
+        //  console.log(2);
+
+        // }
+        // console.log(1);
+        // await page.evaluate(setInputValue)
+        let inputHandles = await page.evaluate(async () => {
+            const inputs = Array.from(document.getElementsByClassName("q-placeholder"))
+            return inputs.map(i => i.value)
+        })
+        console.log(inputHandles);
+
+        const inputValue = ["broker@p2t.sg", "D6i7vcZG"]
+        for (let i in inputHandles) {
+
+            await page.type(".q-placeholder", inputValue)
+        }
+        
+        let res = await page.solveRecaptchas()
+
+        console.log(res);
+       
+
+
+
+
+        await browser.close()
+    } catch (error) {
+        console.log(error);
+    }
 })
 
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-const { executablePath } = require('puppeteer'); 
-
-(async () => {
-  const pathToExtension = require('path').join(__dirname, '2captcha-solver');
-  puppeteer.use(StealthPlugin())
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: [
-      `--disable-extensions-except=${pathToExtension}`,
-      `--load-extension=${pathToExtension}`,
-    ],
-    executablePath: executablePath()
-  });
-  
-  const [page] = await browser.pages()
-
-  await page.goto("https://portal.threetrader.com/login")
-
-  await page.waitForSelector("#recaptcha-anchor")
-  await page.click('#recaptcha-anchor') 
-  await page.waitForSelector(`#recaptcha-anchor[data-state="solved"]`,{timeout:18000}) 
-
-})();
-
+app.listen(PORT, () => {
+    console.log(PORT);
+})
